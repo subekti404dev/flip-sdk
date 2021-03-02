f793d6c413a2e7c0313eb20910fb5f500a7c4708
Urip Subekti <urip.subekti13@gmail.com>
2021-03-03 00:10:50 +0700

    fix digital product

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const { TransactionService } = require('./services/transaction.service')
 const { InquiryService } = require('./services/inquiry.service')
 const { ForwardTransferService } = require('./services/forward-transfer.service')
 const { BreakService } = require('./services/break.service')
+const { WithdrawalService } = require('./services/withdrawal.service')
+const { DigitalProductTransferService } = require('./services/digital-product-transfer.service')
 
 // LoginService.login(process.env.EMAIL, process.env.PASSWORD).then(console.log).catch(console.log)
 // LoginService.refreshToken().then(console.log).catch(console.log)
@@ -15,38 +17,43 @@ const { BreakService } = require('./services/break.service')
 // TransactionService.getTransactions(1, 3).then((data) => {console.log(data)}).catch(console.log)
 
 async function main() {
-  const isBreak = await BreakService.isBreak();
-  console.log({isBreak});
-  return isBreak
-  const recipient = {
-    bank: 'ovo',
-    accountNumber: '087722171686'
-  };
-  const amount = 10000;
-  const remark = 'tes isi ovo lewat flip';
-  const data = await InquiryService.inquiryAndValidate(
-    recipient.bank,
-    recipient.accountNumber,
-    amount,
-    remark
-  );
-  return data;
-  const { inquiryData, validateData } = data;
-  if (inquiryData.status == 'SUCCESS' && validateData.validation_result) {
-    // console.log('OK')
-    const { name } = inquiryData;
-    const transferData = await ForwardTransferService.transfer(
-      recipient.accountNumber,
-      name,
-      recipient.bank,
-      amount,
-      remark,
-      'bca'
-    )
-    return transferData;
-  } else {
-    throw new Error('Something wrong !!');
-  }
+  // const result = await DigitalProductTransferService.transfer(
+  //   'bca', 'ovo_20', '087722171686'
+  // )
+  const result = await DigitalProductTransferService.getWalletProducts()
+  return result
+  // const isBreak = await BreakService.isBreak();
+  // console.log({isBreak});
+  // return isBreak
+  // const recipient = {
+  //   bank: 'cimb',
+  //   accountNumber: '8099087722171686'
+  // };
+  // const amount = 10000;
+  // const remark = 'Imanuel Urip Subek';
+  // const data = await InquiryService.inquiryAndValidate(
+  //   recipient.bank,
+  //   recipient.accountNumber,
+  //   amount,
+  //   remark
+  // );
+  // return data;
+  // const { inquiryData, validateData } = data;
+  // if (inquiryData.status == 'SUCCESS' && validateData.validation_result) {
+  //   // console.log('OK')
+  //   const { name } = inquiryData;
+  //   const transferData = await ForwardTransferService.transfer(
+  //     recipient.accountNumber,
+  //     name,
+  //     recipient.bank,
+  //     amount,
+  //     remark,
+  //     'bca'
+  //   )
+  //   return transferData;
+  // } else {
+  //   throw new Error('Something wrong !!');
+  // }
 
 }
 
---
+++ src/services/digital-product-transfer.service.js
@@ -0,0 +1,82 @@
+const { HttpAuth } = require('../utils/http-auth');
+const _ = require("lodash");
+
+class DigitalProductTransferService {
+  static _http = new HttpAuth();
+
+  static async getWalletProducts() {
+    return this._http.get('v2/digital-products?product_type=5')
+  }
+
+  static async getMinimumPrice(flip_product_id) {
+    try {
+      const payload = {
+        sender_bank: '',
+        flip_product_id,
+        account_number: '087722171686',
+        price: '0'
+      }
+      const FTData = await this._http.post('v2/digital-product-transfers', payload);
+
+    } catch (error) {
+      const message = _.get(error, 'errors[0].message');
+      if (!message) {
+        throw error;
+      } else {
+        const minimum_price = message.match(/\d+/g);
+        if (minimum_price && minimum_price.length > 0) return parseInt(minimum_price[0]);
+        throw message;
+      }
+    }
+  }
+
+  static async transfer(
+    sender_bank,
+    flip_product_id,
+    account_number,
+    price = null
+  ) {
+    if (!price) {
+      price = await this.getMinimumPrice(flip_product_id);
+    }
+    const payload = {
+      sender_bank,
+      flip_product_id,
+      account_number,
+      price
+    }
+    const FTData = await this._http.post('v2/digital-product-transfers', payload);
+    if (FTData && FTData.flip_receiver_bank_code) {
+      const flip_receiver_bank = await this.flipBank(FTData.flip_receiver_bank_code);
+      FTData.flip_receiver_bank = flip_receiver_bank;
+    }
+    return FTData;
+  }
+
+  static async detail(id) {
+    if (typeof (id) == 'string') {
+      id = id.replace("FT", "");
+    }
+    return await this._http.get(`v2/digital-product-transfers/${id}`);
+  }
+
+  static async flipBank(id) {
+    return this._http.get(`v1/bank-code/${id}`);
+  }
+
+  static async confirm(id) {
+    if (typeof (id) == 'string') {
+      id = id.replace("FT", "");
+    }
+    return this._http.put(`v2/digital-product-transfers/${id}/confirm`)
+  }
+
+  static async cancel(id) {
+    if (typeof (id) == 'string') {
+      id = id.replace("FT", "");
+    }
+    return this._http.put(`v2/digital-product-transfers/${id}/cancel`)
+  }
+}
+
+module.exports = { DigitalProductTransferService }
